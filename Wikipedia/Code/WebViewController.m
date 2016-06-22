nambatee/wikipedia-ios

use data-file-width/height with fallback on width/height

Wikipedia/Code/WebViewController.m
@@ -634,9 +634,9 @@ NSString* const WMFCCBySALicenseURL =
             if (!self) {
                 return;
             }
-
-            NSNumber* imageWidth = payload[@"width"];
-            NSNumber* imageHeight = payload[@"height"];
+            
+            NSNumber* imageWidth = payload[@"data-file-width"] ? : payload[@"width"];
+            NSNumber* imageHeight = payload[@"data-file-height"] ? : payload[@"height"];
             CGSize imageSize = CGSizeMake(imageWidth.floatValue, imageHeight.floatValue);
             if (![MWKImage isSizeLargeEnoughForGalleryInclusion:imageSize]) {
                 return;

Requime31/WikipediaAppTest

Wikipedia/Code/WebViewController.m
@@ -151,9 +151,10 @@ NSString* const WMFCCBySALicenseURL =
                 }
             }
         } else if (message.body[@"imageClicked"]) {
-            NSNumber* imageWidth  = message.body[@"imageClicked"][@"width"];
-            NSNumber* imageHeight = message.body[@"imageClicked"][@"height"];
-            CGSize imageSize      = CGSizeMake(imageWidth.floatValue, imageHeight.floatValue);
+            NSDictionary* imageClicked = message.body[@"imageClicked"];
+            NSNumber* imageWidth       = imageClicked[@"data-file-width"] ? : imageClicked[@"width"];
+            NSNumber* imageHeight      = imageClicked[@"data-file-height"] ? : imageClicked[@"height"];
+            CGSize imageSize           = CGSizeMake(imageWidth.floatValue, imageHeight.floatValue);
             if (![MWKImage isSizeLargeEnoughForGalleryInclusion:imageSize]) {
                 return;
             }