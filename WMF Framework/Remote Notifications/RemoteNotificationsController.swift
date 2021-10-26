wikimedia/wikipedia-ios

Add ability to locally page, rework data flow to preserve view model states

WMF Framework/Remote Notifications/RemoteNotificationsController.swift
@@ -19,22 +19,29 @@ import CocoaLumberjackSwift
             DDLogError("Failure deleting legacy RemoteNotifications database files: \(error)")
         }
     }
-    
-    public func importNotificationsIfNeeded(_ completion: @escaping () -> Void) {
+        
+    public func importNotificationsIfNeeded(_ completion: @escaping (RemoteNotificationsImportError?) -> Void) {
         operationsController.importNotificationsIfNeeded(completion)
     }
     
-    public func fetchedResultsController() -> NSFetchedResultsController<RemoteNotification>? {
+    public func fetchNotifications(fetchLimit: Int = 50, fetchOffset: Int = 0) -> [RemoteNotification] {
+        assert(Thread.isMainThread)
         
         guard let viewContext = self.viewContext else {
-            return nil
+            DDLogError("Failure fetching notifications from persistence: missing viewContext")
+            return []
         }
         
         let fetchRequest: NSFetchRequest<RemoteNotification> = RemoteNotification.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
-
-        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: viewContext, sectionNameKeyPath: nil, cacheName: nil)
-
-        return fetchedResultsController
+        fetchRequest.fetchLimit = fetchLimit
+        fetchRequest.fetchOffset = fetchOffset
+        
+        do {
+            return try viewContext.fetch(fetchRequest)
+        } catch {
+            DDLogError("Failure fetching notifications from persistence: \(error)")
+            return []
+        }
     }
 }

Wikipedia/Code/NotificationsCenterViewModel.swift
@@ -1,7 +1,13 @@
 import Foundation
+import CocoaLumberjackSwift
 
 protocol NotificationCenterViewModelDelegate: AnyObject {
-	func collectionViewUpdaterDidUpdate()
+    func cellViewModelsDidChange(cellViewModels: [NotificationsCenterCellViewModel])
+    func reloadCellWithViewModelIfNeeded(_ viewModel: NotificationsCenterCellViewModel)
+}
+
+enum NotificationsCenterSection {
+  case main
 }
 
 @objc
@@ -10,13 +16,19 @@ final class NotificationsCenterViewModel: NSObject {
     // MARK: - Properties
 
     let remoteNotificationsController: RemoteNotificationsController
-
-    fileprivate let fetchedResultsController: NSFetchedResultsController<RemoteNotification>?
-    fileprivate var collectionViewUpdater: CollectionViewUpdater<RemoteNotification>?
-
     weak var delegate: NotificationCenterViewModelDelegate?
 
     private let languageLinkController: MWKLanguageLinkController
+    lazy private var modelController = NotificationsCenterModelController(languageLinkController: self.languageLinkController, delegate: self)
+    
+    private var isPagingEnabled = true
+    var editMode = false {
+        didSet {
+            if oldValue != editMode {
+                modelController.updateCurrentCellViewModelsWith(editMode: editMode)
+            }
+        }
+    }
 
     // MARK: - Lifecycle
 
@@ -25,51 +37,89 @@ final class NotificationsCenterViewModel: NSObject {
         self.remoteNotificationsController = remoteNotificationsController
         self.languageLinkController = languageLinkController
 
-        fetchedResultsController = remoteNotificationsController.fetchedResultsController()
-
-		// TODO: DM-Remove
-		remoteNotificationsController.importNotificationsIfNeeded {}
+        super.init()
 	}
-
-    // MARK: - Public
-
-    func fetchNotifications(collectionView: UICollectionView) {
-        guard let fetchedResultsController = fetchedResultsController else {
+    
+    @objc func contextObjectsDidChange(_ notification: NSNotification) {
+        
+        let refreshedNotifications = notification.userInfo?[NSRefreshedObjectsKey] as? Set<RemoteNotification> ?? []
+        let newNotifications = notification.userInfo?[NSInsertedObjectsKey] as? Set<RemoteNotification> ?? []
+        
+        guard (refreshedNotifications.count > 0 || newNotifications.count > 0) else {
             return
         }
-
-        collectionViewUpdater = CollectionViewUpdater(fetchedResultsController: fetchedResultsController, collectionView: collectionView)
-        collectionViewUpdater?.delegate = self
-        collectionViewUpdater?.performFetch()
-    }
-
-    var numberOfSections: Int {
-        return fetchedResultsController?.sections?.count ?? 0
-    }
-
-    func numberOfItems(section: Int) -> Int {
-        return fetchedResultsController?.sections?[section].numberOfObjects ?? 0
-    }
-
-    func cellViewModel(indexPath: IndexPath) -> NotificationsCenterCellViewModel? {
         
-        if let remoteNotification =  fetchedResultsController?.object(at: indexPath) {
-            return NotificationsCenterCellViewModel(notification: remoteNotification, languageLinkController: languageLinkController)
-        }
+        modelController.addNewCellViewModelsWith(notifications: Array(newNotifications), editMode: self.editMode)
+        modelController.updateCurrentCellViewModelsWith(updatedNotifications: Array(refreshedNotifications), editMode: self.editMode)
+        self.delegate?.cellViewModelsDidChange(cellViewModels: modelController.sortedCellViewModels)
+    }
 
-        return nil
+    // MARK: - Public
+    
+    func fetchFirstPage() {
+        kickoffImportIfNeeded { [weak self] in
+            
+            DispatchQueue.main.async {
+                guard let self = self else {
+                    return
+                }
+                
+                let notifications = self.remoteNotificationsController.fetchNotifications()
+                self.modelController.addNewCellViewModelsWith(notifications: notifications, editMode: self.editMode)
+                self.delegate?.cellViewModelsDidChange(cellViewModels: self.modelController.sortedCellViewModels)
+            }
+        }
+    }
+    
+    func fetchNextPage() {
+        
+        guard isPagingEnabled == true else {
+            DDLogDebug("Request to fetch next page while paging is disabled. Ignoring.")
+            return
+        }
+        
+        let notifications = self.remoteNotificationsController.fetchNotifications(fetchOffset: modelController.fetchOffset)
+        
+        guard notifications.count > 0 else {
+            isPagingEnabled = false
+            return
+        }
+        
+        modelController.addNewCellViewModelsWith(notifications: notifications, editMode: self.editMode)
+        self.delegate?.cellViewModelsDidChange(cellViewModels: modelController.sortedCellViewModels)
+    }
+    
+    func toggleCheckedStatus(cellViewModel: NotificationsCenterCellViewModel) {
+        cellViewModel.toggleCheckedStatus()
+        reloadCellWithViewModelIfNeeded(viewModel: cellViewModel)
     }
 
 }
 
-extension NotificationsCenterViewModel: CollectionViewUpdaterDelegate {
+private extension NotificationsCenterViewModel {
+    func kickoffImportIfNeeded(completion: @escaping () -> Void) {
+        remoteNotificationsController.importNotificationsIfNeeded() { [weak self] error in
+            
+            guard let self = self else {
+                return
+            }
+            
+            if let error = error,
+               error == RemoteNotificationsImportError.failureSettingUpModelController {
+                //TODO: trigger error state of some sort
+                completion()
+                return
+            }
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(self.contextObjectsDidChange(_:)), name: Notification.Name.NSManagedObjectContextObjectsDidChange, object: self.remoteNotificationsController.viewContext)
 
-    func collectionViewUpdater<T>(_ updater: CollectionViewUpdater<T>, didUpdate collectionView: UICollectionView) where T : NSFetchRequestResult {
-        delegate?.collectionViewUpdaterDidUpdate()
+            completion()
+        }
     }
+}
 
-    func collectionViewUpdater<T>(_ updater: CollectionViewUpdater<T>, updateItemAtIndexPath indexPath: IndexPath, in collectionView: UICollectionView) where T : NSFetchRequestResult {
-
+extension NotificationsCenterViewModel: NotificationsCenterModelControllerDelegate {
+    func reloadCellWithViewModelIfNeeded(viewModel: NotificationsCenterCellViewModel) {
+        delegate?.reloadCellWithViewModelIfNeeded(viewModel)
     }
-
 }

Requime31/WikipediaAppTest

WMF Framework/Remote Notifications/RemoteNotificationsController.swift
@@ -19,26 +19,33 @@ import CocoaLumberjackSwift
             DDLogError("Failure deleting legacy RemoteNotifications database files: \(error)")
         }
     }
-    
-    public func importNotificationsIfNeeded(_ completion: @escaping () -> Void) {
+        
+    public func importNotificationsIfNeeded(_ completion: @escaping (RemoteNotificationsOperationsError?) -> Void) {
         operationsController.importNotificationsIfNeeded(completion)
     }
     
-    public func refreshNotifications(_ completion: @escaping () -> Void) {
+    public func refreshNotifications(_ completion: @escaping (RemoteNotificationsOperationsError?) -> Void) {
         operationsController.refreshNotifications(completion)
     }
     
-    public func fetchedResultsController() -> NSFetchedResultsController<RemoteNotification>? {
+    public func fetchNotifications(fetchLimit: Int = 50, fetchOffset: Int = 0) -> [RemoteNotification] {
+        assert(Thread.isMainThread)
         
         guard let viewContext = self.viewContext else {
-            return nil
+            DDLogError("Failure fetching notifications from persistence: missing viewContext")
+            return []
         }
         
         let fetchRequest: NSFetchRequest<RemoteNotification> = RemoteNotification.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
-
-        let fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: viewContext, sectionNameKeyPath: nil, cacheName: nil)
-
-        return fetchedResultsController
+        fetchRequest.fetchLimit = fetchLimit
+        fetchRequest.fetchOffset = fetchOffset
+        
+        do {
+            return try viewContext.fetch(fetchRequest)
+        } catch {
+            DDLogError("Failure fetching notifications from persistence: \(error)")
+            return []
+        }
     }
 }

Wikipedia/Code/NotificationsCenterViewModel.swift
@@ -1,7 +1,13 @@
 import Foundation
+import CocoaLumberjackSwift
 
 protocol NotificationCenterViewModelDelegate: AnyObject {
-	func collectionViewUpdaterDidUpdate()
+    func cellViewModelsDidChange(cellViewModels: [NotificationsCenterCellViewModel])
+    func reloadCellWithViewModelIfNeeded(_ viewModel: NotificationsCenterCellViewModel)
+}
+
+enum NotificationsCenterSection {
+  case main
 }
 
 @objc
@@ -10,13 +16,19 @@ final class NotificationsCenterViewModel: NSObject {
     // MARK: - Properties
 
     let remoteNotificationsController: RemoteNotificationsController
-
-    fileprivate let fetchedResultsController: NSFetchedResultsController<RemoteNotification>?
-    fileprivate var collectionViewUpdater: CollectionViewUpdater<RemoteNotification>?
-
     weak var delegate: NotificationCenterViewModelDelegate?
 
     private let languageLinkController: MWKLanguageLinkController
+    lazy private var modelController = NotificationsCenterModelController(languageLinkController: self.languageLinkController, delegate: self)
+    
+    private var isPagingEnabled = true
+    var editMode = false {
+        didSet {
+            if oldValue != editMode {
+                modelController.updateCurrentCellViewModelsWith(editMode: editMode)
+            }
+        }
+    }
 
     // MARK: - Lifecycle
 
@@ -25,57 +37,95 @@ final class NotificationsCenterViewModel: NSObject {
         self.remoteNotificationsController = remoteNotificationsController
         self.languageLinkController = languageLinkController
 
-        fetchedResultsController = remoteNotificationsController.fetchedResultsController()
-
-		// TODO: DM-Remove
-		remoteNotificationsController.importNotificationsIfNeeded {}
+        super.init()
 	}
     
-    func refreshNotifications() {
-        remoteNotificationsController.refreshNotifications {
-            //TODO: Set any refreshing loading states here
+    @objc func contextObjectsDidChange(_ notification: NSNotification) {
+        
+        let refreshedNotifications = notification.userInfo?[NSRefreshedObjectsKey] as? Set<RemoteNotification> ?? []
+        let newNotifications = notification.userInfo?[NSInsertedObjectsKey] as? Set<RemoteNotification> ?? []
+        
+        guard (refreshedNotifications.count > 0 || newNotifications.count > 0) else {
+            return
         }
+        
+        modelController.addNewCellViewModelsWith(notifications: Array(newNotifications), editMode: self.editMode)
+        modelController.updateCurrentCellViewModelsWith(updatedNotifications: Array(refreshedNotifications), editMode: self.editMode)
+        self.delegate?.cellViewModelsDidChange(cellViewModels: modelController.sortedCellViewModels)
     }
 
     // MARK: - Public
-
-    func fetchNotifications(collectionView: UICollectionView) {
-        guard let fetchedResultsController = fetchedResultsController else {
+    
+    func refreshNotifications() {
+        remoteNotificationsController.refreshNotifications { _ in
+            //TODO: Set any refreshing loading states here
+        }
+    }
+    
+    func fetchFirstPage() {
+        kickoffImportIfNeeded { [weak self] in
+            
+            DispatchQueue.main.async {
+                guard let self = self else {
+                    return
+                }
+                
+                let notifications = self.remoteNotificationsController.fetchNotifications()
+                self.modelController.addNewCellViewModelsWith(notifications: notifications, editMode: self.editMode)
+                self.delegate?.cellViewModelsDidChange(cellViewModels: self.modelController.sortedCellViewModels)
+            }
+        }
+    }
+    
+    func fetchNextPage() {
+        
+        guard isPagingEnabled == true else {
+            DDLogDebug("Request to fetch next page while paging is disabled. Ignoring.")
             return
         }
-
-        collectionViewUpdater = CollectionViewUpdater(fetchedResultsController: fetchedResultsController, collectionView: collectionView)
-        collectionViewUpdater?.delegate = self
-        collectionViewUpdater?.performFetch()
-    }
-
-    var numberOfSections: Int {
-        return fetchedResultsController?.sections?.count ?? 0
-    }
-
-    func numberOfItems(section: Int) -> Int {
-        return fetchedResultsController?.sections?[section].numberOfObjects ?? 0
-    }
-
-    func cellViewModel(indexPath: IndexPath) -> NotificationsCenterCellViewModel? {
         
-        if let remoteNotification =  fetchedResultsController?.object(at: indexPath) {
-            return NotificationsCenterCellViewModel(notification: remoteNotification, languageLinkController: languageLinkController)
+        let notifications = self.remoteNotificationsController.fetchNotifications(fetchOffset: modelController.fetchOffset)
+        
+        guard notifications.count > 0 else {
+            isPagingEnabled = false
+            return
         }
-
-        return nil
+        
+        modelController.addNewCellViewModelsWith(notifications: notifications, editMode: self.editMode)
+        self.delegate?.cellViewModelsDidChange(cellViewModels: modelController.sortedCellViewModels)
+    }
+    
+    func toggleCheckedStatus(cellViewModel: NotificationsCenterCellViewModel) {
+        cellViewModel.toggleCheckedStatus()
+        reloadCellWithViewModelIfNeeded(viewModel: cellViewModel)
     }
 
 }
 
-extension NotificationsCenterViewModel: CollectionViewUpdaterDelegate {
+private extension NotificationsCenterViewModel {
+    func kickoffImportIfNeeded(completion: @escaping () -> Void) {
+        remoteNotificationsController.importNotificationsIfNeeded() { [weak self] error in
+            
+            guard let self = self else {
+                return
+            }
+            
+            if let error = error,
+               error == RemoteNotificationsOperationsError.failureSettingUpModelController {
+                //TODO: trigger error state of some sort
+                completion()
+                return
+            }
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(self.contextObjectsDidChange(_:)), name: Notification.Name.NSManagedObjectContextObjectsDidChange, object: self.remoteNotificationsController.viewContext)
 
-    func collectionViewUpdater<T>(_ updater: CollectionViewUpdater<T>, didUpdate collectionView: UICollectionView) where T : NSFetchRequestResult {
-        delegate?.collectionViewUpdaterDidUpdate()
+            completion()
+        }
     }
+}
 
-    func collectionViewUpdater<T>(_ updater: CollectionViewUpdater<T>, updateItemAtIndexPath indexPath: IndexPath, in collectionView: UICollectionView) where T : NSFetchRequestResult {
-
+extension NotificationsCenterViewModel: NotificationsCenterModelControllerDelegate {
+    func reloadCellWithViewModelIfNeeded(viewModel: NotificationsCenterCellViewModel) {
+        delegate?.reloadCellWithViewModelIfNeeded(viewModel)
     }
-
 }